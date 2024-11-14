ac2e03bc48aa9ac0bf3602cd5b5d51c7ac76b743
Rhys Miller <rjmills87@aol.com>
2024-11-14 22:37:46 +0000

    Installed and configured shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
-    <h1 className="text-teal-500 text-3xl font-bold underline">
-      Hello Team 23!
-    </h1>
+    <>
+      <h1 className="text-teal-500 text-3xl font-bold underline">
+        Hello Team 23!
+      </h1>
+      <Button>ShadCN</Button>
+    </>
   );
 }
