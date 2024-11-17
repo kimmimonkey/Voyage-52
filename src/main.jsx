52626ac09dab44e73ce1bed49f79ec694d2dd1c6
Rhys Miller <rjmills87@aol.com>
2024-11-17 20:36:16 +0000

    Created Header component with placeholder content

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Header>
+        <App />
+      </Header>
     </BrowserRouter>
   </StrictMode>
 );
