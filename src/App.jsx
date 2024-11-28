c091095a74ce86baf6e477a1e14dad6be649edcf
Rhys Miller <rjmills87@aol.com>
2024-11-28 15:03:23 +0000

    Removed unused import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/Header";
-import { Toaster } from "./components/ui/toaster";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
